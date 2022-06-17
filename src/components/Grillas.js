c8012a697d72e0d39b09a6294c0f8e751da1566a
Temo17X <floresedut17@gmail.com>
2022-06-17 02:14:24 -0500

    cambio en mi proyecto

--- src/components/Grillas.js
+++ src/components/Grillas.js
@@ -4,37 +4,51 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import senuez from '../assets/img/senuez.jpeg';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import '../Grillas.css';
+import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(2),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
-export default function CSSGrid() {
+export default function About() {
   return (
-    <Box sx={{ width: 1 }}>
-      <Box display="grid" gridTemplateColumns="repeat(24, 1fr)" gap={2}>
-
-
-        <Box gridColumn="span 4">
-          <Item>
-              <h1>Foto de perfil</h1>
+    <div>
+      <Box sx={{ flexGrow: 1 }} style={{padding:'1rem'}}>
+      <Item>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <Item>
+          <h1>Foto de perfil</h1>
+          <h5>Todas las caras son hermosas</h5>
               <img src={senuez}/>
-              
-          </Item>
-        </Box>
+              <br></br>
+              <p>Foto de perfil</p>
+              <br></br>
+              <p> .</p>  
+              </Item>  
+           </Grid>
 
-
-        <Box gridColumn="span 13">
-          <Item>
-
-            <h1>Información Personal</h1>
+          <Grid item xs={5}>
+            <Item>
+          <h1>Información Personal</h1>
+          <h5>Es escencial al momento de venderte</h5>
             <br></br>
-            <p>Pasen porno por prontipagos</p>
+            
+           
             <Box
               component="form"
               sx={{
@@ -45,20 +59,29 @@ export default function CSSGrid() {
                >
                  <TextField id="outlined-basic" label="Nombre completo" variant="outlined" />
                  <TextField id="outlined-basic" label="Edad" variant="outlined" />
+                 
                  <br></br>
                  <TextField id="outlined-basic" label="Lugar de Nacimiento" variant="outlined" />
-                 <TextField id="outlined-basic" label="Genero" variant="outlined" />
-         </Box>
-         <br></br>
-         <br></br>
-          </Item>
-        </Box>
+                 <FormControl>
+                  <FormLabel id="demo-row-radio-buttons-group-label">Género</FormLabel>
+                  <RadioGroup
+                   row
+                   aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                    >
+                  <FormControlLabel value="Masculino" control={<Radio />} label="Masculino" /> 
+                  <FormControlLabel value="Femenino" control={<Radio />} label="Femenino" />
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+              </Item>
+          </Grid>
 
 
-        <Box gridColumn="span 7">
+          <Grid item xs={4}>
           <Item>
-
-              <h1>Contacto</h1>
+          <h1>Contacto</h1>
+          <h5>La comunicación es vida</h5>
               <Box
               component="form"
               sx={{
@@ -67,26 +90,25 @@ export default function CSSGrid() {
                   noValidate
                  autoComplete="off"
                >
-                 <TextField id="outlined-basic" label="Correo Electronico:" variant="outlined" />
+                 <TextField id="outlined-basic" label="Correo Electronico Escolar:" variant="outlined" />
+
+                 <TextField id="outlined-basic" label="Correo Electronico Personal:" variant="outlined" />
                  <br></br>
-                 <TextField id="outlined-basic" label="Telefono:" variant="outlined" />
-                 
-         </Box>
-         <br></br>
-         <br></br>
-         <br></br>
-          </Item>
-        </Box>
+                 <TextField id="outlined-basic" label="Telefono Fijo:" variant="outlined" />
+                 <TextField id="outlined-basic" label="Telefono Móvil:" variant="outlined" />   
+             </Box>
+             </Item>
+          
+          </Grid>
 
-
-        <Box gridColumn="span 7">
-          <Item>
-
-            <h1>Educación</h1>
+          <Grid item xs={3}>
+            <Item>
+          <h1>Educación</h1>
+          <h5>Tu trayectora escolar</h5>
             <Box
               component="form"
               sx={{
-                '& > :not(style)': { m: 1, width: '35ch' },
+                '& > :not(style)': { m: 1, width: '23ch' },
                  }}
                   noValidate
                  autoComplete="off"
@@ -99,16 +121,15 @@ export default function CSSGrid() {
                  <TextField id="outlined-basic" label="Titulado o Pasante" variant="outlined" />
                  <br></br>
                  <TextField id="outlined-basic" label="Periodo Cursado" variant="outlined" />
-                 
-         </Box>
-          </Item>
-        </Box>
+                  
+            </Box>
+            </Item>   
+           </Grid>
 
-
-        <Box gridColumn="span 8">
-          <Item>
-
-            <h1>Experiencia Laboral</h1>
+           <Grid item xs={5}>
+           <Item>
+           <h1>Experiencia Laboral</h1>
+           <h5>Tu trayectoria laboral</h5>
             <Box
               component="form"
               sx={{
@@ -124,39 +145,41 @@ export default function CSSGrid() {
                  <br></br>
                  <TextField id="outlined-basic" label="Periodo laborado" variant="outlined" />
          </Box>
-          </Item>
-        </Box>
+         </Item>
+           </Grid>
 
 
-        <Box gridColumn="span 8">
-          <Item>
-
-            <h1>Skills</h1>
-            <h2>Cursos</h2>
+           <Grid item xs={4}>
+             <Item>
+           <h1>Skills</h1>
+           <h5>Complementa tu CV</h5>
             <Box
               component="form"
               sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
+                '& > :not(style)': { m: 1, width: '15ch' },
                  }}
                   noValidate
                  autoComplete="off"
                >
                 
                  <TextField id="outlined-basic" label="Curso 1" variant="outlined" />
-                 <TextField id="outlined-basic" label="Años" variant="outlined" />
+                 <TextField id="outlined-basic" label="Años XP" variant="outlined" />
                  <br></br>
                  <TextField id="outlined-basic" label="Curso 2" variant="outlined" />
-                 <TextField id="outlined-basic" label="Años" variant="outlined" />
+                 <TextField id="outlined-basic" label="Años XP" variant="outlined" />
                  <br></br>
                  <TextField id="outlined-basic" label="Curso 3" variant="outlined" />
-                 <TextField id="outlined-basic" label="Años" variant="outlined" />
+                 <TextField id="outlined-basic" label="Años XP" variant="outlined" />
                  
          </Box>
-          </Item>
-        </Box>
+         </Item>
+           </Grid>
 
-
+        </Grid>
+        </Item>
       </Box>
-    </Box>
+    </div>
+    
   );
-}
+  }
+
